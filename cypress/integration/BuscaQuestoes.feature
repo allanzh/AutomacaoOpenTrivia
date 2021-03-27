cc1104875517ee907b33e6314dbd589c7f160321
allanOliveiraRamos <allancleysson@gmail.com>
2021-03-27 16:25:38 -0300

    implementação do cenário Busca por questão inexistente

--- cypress/integration/BuscaQuestoes.feature
+++ cypress/integration/BuscaQuestoes.feature
@@ -4,7 +4,7 @@ Funcionalidade: Busca no Banco de Questões
     Para que possa encontrar resultados das pesquisas feitas
 
 Cenario: Busca por questão inexistente
-    Given que navego para a página de busca do banco de questões
-    And digito "Science: Computers" no campo de busca
-    When clico no botão de buscar
-    Then visualizo uma mensagem de erro com o texto "No questions found."
+    Dado que navego para a página de busca do banco de questões
+    E digito "Science: Computers" no campo de busca
+    Quando clico no botão de buscar
+    Então visualizo uma mensagem de erro com o texto "No questions found."
--- cypress/integration/BuscaQuestoes/BuscaQuestoesSeps.js
+++ cypress/integration/BuscaQuestoes/BuscaQuestoesSeps.js
@@ -1,19 +1,18 @@
 /// <reference types="cypress" />
 
 Given(/^que navego para a página de busca do banco de questões$/, () => {
-	return true;
+	cy.visit('/');
+    cy.get('a[href$="browse.php"]').first().click();
 });
 
-(/^digito "([^"]*)" no campo de busca$/, (args1) => {
-	console.log(args1);
-	return true;
+And(/^digito "([^"]*)" no campo de busca$/, (termoBusca) => {
+	cy.get('#query').type(termoBusca);
 });
 
 When(/^clico no botão de buscar$/, () => {
-	return true;
+    cy.get('.form-inline button').click();
 });
 
-Then(/^visualizo uma mensagem de erro com o texto "([^"]*)"$/, (args1) => {
-	console.log(args1);
-	return true;
+Then(/^visualizo uma mensagem de erro com o texto "([^"]*)"$/, (msgErro) => {
+	cy.get('.alert').should('have.text', msgErro);
 });
